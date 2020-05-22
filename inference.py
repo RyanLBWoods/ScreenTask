d8263664e93e28b211dc57212fbacf5884b37988
RyanLBWoods <806331786@qq.com>
2020-05-22 14:19:20 +0800

    update simpleInfer

--- inference.py
+++ inference.py
@@ -1,14 +1,17 @@
 import torch
+import torch.utils.data as data
+from torch.autograd import Variable
 from torch.nn.functional import log_softmax
+from classifier import simpleClassifier
+from data_loader import dataloader
 
 
 class simpleInfer(object):
     def __init__(self):
-        self.load_model()
         self.model = None
 
-    def load_model(model_path: str):
-        model = torch.load(model_path)
+    def load_model(self, model_path: str):
+        self.model = torch.load(model_path)
 
     def infer(self, inp: list):
         """
@@ -19,16 +22,26 @@ class simpleInfer(object):
         :return:
         - list, a list of int, which is th result of model inference
         """
-        assert self.model is not None, "Model is not loaded!"
-        outputs = self.model(inp)
-        outputs = log_softmax(outputs, dim=1)
-        pred = torch.max(outputs, dim=1)[1]
-        print('The prediction is ' + pred.item())
-        return pred.item()
+        assert self.model is not None, "Model is not loaded! Please call load_model()"
+        self.model.eval()
+        with torch.no_grad():
+            inputs = torch.from_numpy(inp)
+            outputs = self.model(inputs)
+            outputs = log_softmax(outputs, dim=1)
+            preds = torch.max(outputs, dim=1)[1]
+        return preds.tolist()
 
 
-if __name__ == '__main__':
-    path = './simpleClassifier_20.pth'
-    infer = simpleInfer()
-    infer.load_model(path)
-    pred = infer.infer()
+# if __name__ == '__main__':
+#     path = './t10k-images-idx3-ubyte'
+#     l_path = './t10k-labels-idx1-ubyte'
+#     test_data = data.DataLoader(dataloader(path, l_path), batch_size=20, shuffle=True)
+#     path = './simpleClsifier_model.pth'
+#     infer = simpleInfer()
+#     infer.load_model(path)
+#     for imgs, labels in test_data:
+#         imgs = imgs.numpy()
+#         outputs = infer.infer(imgs)
+#         for i in range(20):
+#             print(outputs[i], labels[i])
+#         exit(0)
